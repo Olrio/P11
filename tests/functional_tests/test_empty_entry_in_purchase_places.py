934fd47beb99543b711c4c4e611f0c5160182248
Olrio <opm.rio@gmail.com>
2022-10-09 15:06:56 +0200

    intégration des fonctionnalités setUp et tearDown au sein de la classe de test

--- tests/functional_tests/test_empty_entry_in_purchase_places.py
+++ tests/functional_tests/test_empty_entry_in_purchase_places.py
@@ -3,20 +3,7 @@ from flask_testing import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from P11.server import app, clubs, competitions
-
-
-def setup_module():
-    global club, competition, initial_points, places_left
-    club = list(filter(lambda x: x['name'] == 'Simply Lift', clubs))[0]
-    competition = list(filter(lambda x: x['name'] == 'Spring Festival', competitions))[0]
-    competition['date'] = "2025-03-27 10:00:00"
-    initial_points = int(club["points"])
-    places_left = competition['numberOfPlaces']
-
-
-def teardown_module():
-    club['points'] = str(initial_points)
-    competition['numberOfPlaces'] = places_left
+import datetime
 
 
 class TestPlacesInput(LiveServerTestCase):
@@ -25,9 +12,20 @@ class TestPlacesInput(LiveServerTestCase):
         app.config['TESTING'] = True
         return app
 
+    @classmethod
+    def setUpClass(cls):
+        cls.club = next(filter(lambda x: x['name'] == 'Simply Lift', clubs))
+        cls.competition = next(filter(lambda x: x['name'] == 'Spring Festival', competitions))
+        cls.competition['date'] = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(days=100),
+                                                         "%Y-%m-%d %H:%M:%S")
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
     def test_enter_empty_entry_should_display_required_message(self):
         browser = webdriver.Firefox()
-        browser.get(self.get_server_url() + url_for("book", competition=competition['name'], club=club['name']))
+        browser.get(self.get_server_url() + url_for("book", competition=self.competition['name'], club=self.club['name']))
         places = browser.find_element(By.NAME, 'places')
         submit = browser.find_element(By.XPATH, '//button')
         submit.click()
